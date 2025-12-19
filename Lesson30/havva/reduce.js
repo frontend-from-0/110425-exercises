949403db00e608dc2c58890f743b04dda63dde68
Havva Nur Unal <havvanurunal207@icloud.com>
2025-12-19 12:10:22 +0300

    Lesson 30

---
+++ Lesson30/havva/reduce.js
@@ -0,0 +1,95 @@
+/* array.reduce(callback(accumulator, currentValue, currentIndex, array), initialValue);
+  
+- callback: A function that is executed on each element of the array. It takes four arguments:
+- accumulator: The accumulated value that is returned after each iteration. It starts with the value of initialValue on the first iteration and is the result of the previous iteration in subsequent iterations.
+- currentValue: The current element being processed in the array.
+- currentIndex: The index of the current element being processed.
+- array: The array on which reduce() was called.
+
+- initialValue: An optional initial value for the accumulator. If not provided, the first element of the array is used as the initial value and the iteration starts from the second element.
+*/
+
+// Exercise 1:
+// Calculate the sum of all numbers in the given array.
+const numbersEx1 = [1, 2, 3, 4, 5];
+
+const result1 = numbersEx1.reduce(
+  (accumulator, currentValue, currentIndex, array) => {
+    console.log(
+      'Accumulator',
+      accumulator,
+      'currentValue',
+      currentValue,
+      'currentIndex',
+      currentIndex,
+      'array',
+      array,
+    );
+
+    const res = accumulator + currentValue;
+    console.log(res);
+    return res;
+  },
+);
+
+// Name of the params could change but the position should always be the same!
+numbersEx1.reduce((acc, val, _, array) => {
+  console.log('Our array is', array);
+  return acc + val;
+});
+
+console.log('Result for ex. 1', result1);
+
+
+
+// Exercise 2:
+// Find the maximum value in the given array. (Hint: Use -Infinity to compare values to)
+const numbersEx2 = [8, 3, 11, 6, 4];
+
+console.log('Result for ex. 2', numbersEx2.reduce((accumulator, currentValue) => {
+  return currentValue > accumulator ? currentValue : accumulator;
+}, -Infinity))
+
+
+
+// Exercise 3: Explain the code
+// Count the occurrences of each element in the given array and return an object with the counts.
+const elements = ['a', 'b', 'a', 'c', 'b', 'a'];
+
+const elementCounts = elements.reduce((accumulator, currentValue) => {
+  if (accumulator[currentValue]) { // checks if the current letter is already a key in our object. 
+    accumulator[currentValue]++; // if it exists it increments the current value of the letter. 
+  } else {
+    accumulator[currentValue] = 1; // if it doesn't exist it creates a key and sets its value to 1. 
+  }
+  return accumulator;
+}, {}); // {} tells JS to start our accumulator as an empty object. 
+
+console.log(elementCounts); // Output: { a: 3, b: 2, c: 1 }
+
+
+// Exercise 4:
+// Calculate the average of all numbers in the given array.
+const numbersEx4 = [10, 20, 30, 40, 50];
+
+const totalSum = numbersEx4.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;;
+}, 0);
+
+const average = totalSum / numbersEx4.length;
+console.log(average);
+
+
+// Exercise 5: Explain the code below
+// Count the number of unique elements in the array.
+const numbersEx5 = [1, 2, 3, 2, 4, 3, 5];
+const numbers = [1, 2, 3, 2, 4, 3, 5];
+
+const uniqueCount = numbers.reduce((accumulator, currentValue) => {
+  if (!accumulator.includes(currentValue)) {
+    accumulator.push(currentValue);
+  }
+  return accumulator;
+}, []).length;
+
+console.log(uniqueCount); // Output: 5
